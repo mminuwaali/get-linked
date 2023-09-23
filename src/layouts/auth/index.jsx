264b03472f717d23d0e1be02f7215899c7772f1b
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2023-09-23 07:29:49 +0100

    configured the auth layout

--- src/layouts/auth/index.jsx
+++ src/layouts/auth/index.jsx
@@ -1,8 +1,26 @@
 import "./style.sass";
+import { useState } from "react";
+import $header from '../../components/header';
 
 // A higher order component that returns a react functional component
 export default function ($element) {
-    return () => <>
-        <$element />
-    </>;
+    return () => {
+        const [uri, setUri] = useState(null);
+
+        return <section id="auth">
+            <$header />
+            <div className="header">
+                <h2 href="#">Register</h2>
+            </div>
+            <div className="auth-container">
+                <aside>
+                    <img src={uri} alt="authentication image" className="auth-image" />
+                </aside>
+
+                <main>
+                    <$element setUri={setUri} />
+                </main>
+            </div>
+        </section>;
+    };
 };
